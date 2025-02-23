2ef2dc04481bd8f7e56f866149264f03c76faeb8
Gutobala <gutobala@gmail.com>
2025-02-22 22:53:37 -0300

    gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ea5502ba1b41f425"; }
+/******/ 		__webpack_require__.h = function() { return "0fa81e32fd17b4aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.db761e76044a40bf.hot-update.js
+++ .next/static/webpack/webpack.db761e76044a40bf.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c99cd808cc05210d"; }
+/******/ 	__webpack_require__.h = function() { return "50eea629beae2661"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
